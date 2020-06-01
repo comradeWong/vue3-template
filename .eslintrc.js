f9fc836fe10e7fe55b6b805a988e2c79cfb621bd
wangxinyu <615948603@qq.com>
2020-06-01 14:04:29 +0800

    feat 引入prettier 修改eslint 引入scss静态资源

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,22 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: jooey wong
+ * @Date: 2020-06-01 11:27:22
+ * @LastEditors: jooey wong
+ * @LastEditTime: 2020-06-01 13:44:57
+ */ 
 module.exports = {
   root: true,
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-};
+}
